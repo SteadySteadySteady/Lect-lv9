c3fa271fdbbc24b45447cb69a14a50ca4d00cdb9
cakalu <cakalu@DESKTOP-KI8DJFN>
2021-10-05 14:38:58 +0900

    aaaaaa

--- atm/src/controller/FileManager.java
+++ atm/src/controller/FileManager.java
@@ -7,7 +7,6 @@ public class FileManager {
 	public static FileManager instance = new FileManager();
 	String filename = "atm.txt";
 	File atm = new File(filename);
-	FileWriter fw = new FileWriter(atm);
 	
 	// 메소드
 	// 저장
