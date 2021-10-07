1f6856303b3287477b0663f38f392d8d4c65e372
cakalu <cakalu@DESKTOP-KI8DJFN>
2021-10-07 13:32:27 +0900

    aaaaaaaaa

--- shop/src/models/User.java
+++ shop/src/models/User.java
@@ -1,5 +1,13 @@
 package models;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	private String id;
+	private String pw;
+	
+	ArrayList<Cart> carts = new ArrayList<>();
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
 }
