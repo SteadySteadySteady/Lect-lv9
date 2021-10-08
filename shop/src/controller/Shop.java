4b901099e1a5e1ffde9311edc4bb336f214f5a1a
cakalu <cakalu@DESKTOP-KI8DJFN>
2021-10-08 15:11:19 +0900

    asdf

--- shop/src/controller/Shop.java
+++ shop/src/controller/Shop.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Shop {
 	UserManager um = UserManager.instance;
 	ItemManager im = ItemManager.instance;
@@ -7,11 +9,30 @@ public class Shop {
 	private void isRun() {
 		this.isRun = true;
 	}
-	
-	
+	private void printMenu() {
+		System.out.println("1.가입 2.탈퇴 3.로그인 4.로그아웃\n100.관리자 0.종료");
+	}
+	private void selectMenu() {
+		System.out.print("입력 : ");
+		int sel = Integer.parseInt(this.um.s.next());
+		if(sel == 1) {
+			
+		} else if(sel == 2) {
+			
+		} else if(sel == 3) {
+			
+		} else if(sel == 4) {
+			
+		} else if(sel == 100) {
+			
+		} else if(sel == 0) {
+			
+		}
+	}
 	public void shopRun() {
 		while(!this.isRun) {
-			
+			printMenu();
+			selectMenu();
 		}
 	}
 	
