f7dddcd5f8195bc45eb1a47df01771d5c7c8414b
cakalu <cakalu@DESKTOP-KI8DJFN>
2021-10-08 09:49:24 +0900

    aaaaaaaaaa

--- shop/src/controller/UserManager.java
+++ shop/src/controller/UserManager.java
@@ -1,9 +1,15 @@
 package controller;
 
+import java.util.ArrayList;
+
 import models.User;
 
 public class UserManager {
 	public static UserManager instance = new UserManager();
+	ArrayList<User> users = new ArrayList<>();
 	
+	public void addUser() {
+		
+	}
 	
 }
