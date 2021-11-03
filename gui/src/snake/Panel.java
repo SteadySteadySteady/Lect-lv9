0e7575418d9aebca229530eb79a183c17440af91
cakalu <cakalu@DESKTOP-KI8DJFN>
2021-11-03 11:42:04 +0900

    end

--- gui/src/snake/Panel.java
+++ gui/src/snake/Panel.java
@@ -13,8 +13,7 @@ public class Panel extends Listener{
 	static int SIZE = 50;
 	private ArrayList<Integer> x;
 	private ArrayList<Integer> y;
-	private int snakeLength = 3;
-	private int targetNum = 1;
+	private int snakeLength = 1;
 	private JButton dirKey[];
 	private Rect map[][];
 	Random r = new Random();
@@ -63,18 +62,19 @@ public class Panel extends Listener{
 		else if(e.VK_RIGHT == e.getKeyCode()) {
 			xx += 1;
 		}
-		if(check(yy, xx)) {
-			map[y.get(0)][x.get(0)].setSnakeNum(0);
-			y.set(0, yy);
-			x.set(0, xx);
-			map[y.get(0)][x.get(0)].setSnakeNum(targetNum);
+		int checkNum = check(yy, xx);
+		if(checkNum == 0) {
 			snakeMove(yy, xx);
-			int rNum = 1;
+			int rNum = r.nextInt(4);
 			if(rNum == 1) {
 				addItem();
 			}
-		} else {
-			System.out.println("ав╬З╢ы");
+		}
+		else if(checkNum == 1) {
+			item(yy, xx);
+		}
+		else if(checkNum == -1){
+			Alert alert = new Alert();
 		}
 	}
 	private void addItem() {
@@ -89,35 +89,51 @@ public class Panel extends Listener{
 	}
 	private void snakeMove(int yy, int xx) {
 		// arrx,y 0 -> arrx,y 1
-		int tempX = xx;
-		int tempY = yy;
-		for(int i = 1; i < snakeLength-1; i += 1) {
-			if(i > 1) {
-				
-			}
-			map[y.get(i)][x.get(i)].setSnakeNum(0);
-			y.set(i, tempY);
-			x.set(i, tempX);
+		map[y.get(0)][x.get(0)].setSnakeNum(0);
+		map[y.get(snakeLength-1)][x.get(snakeLength-1)].setSnakeNum(0);
+		for(int i = snakeLength-1; i > 0 ; i -= 1) {
+			y.set(i, y.get(i-1));
+			x.set(i, x.get(i-1));
+			map[y.get(i)][x.get(i)].setSnakeNum(i+1);
+//			System.out.printf("i = %d, y = %d, x = %d, sNum = %d\n", i, y.get(i), x.get(i), map[y.get(i)][x.get(i)].getSnakeNum());
+		}
+		y.set(0, yy);
+		x.set(0, xx);
+		map[y.get(0)][x.get(0)].setSnakeNum(1);
+	}
+	private int check(int yy, int xx) {
+		if(yy < 0 || yy >= map.length || xx < 0 || xx >= map[0].length || map[yy][xx].getSnakeNum() != 0) {
+			return -1;
+		}
+		else if(map[yy][xx].getItem()) {
+			return 1;
+		}
+		return 0;
+	}
+	
+	private void item(int yy, int xx) {
+		map[yy][xx].setItem(false);
+		snakeLength += 1;
+		y.add(1, y.get(0));
+		x.add(1, x.get(0));
+		y.set(0, yy);
+		x.set(0, xx);
+		for(int i = 0; i < snakeLength; i += 1) {
 			map[y.get(i)][x.get(i)].setSnakeNum(i+1);
 		}
-	}
-	private boolean check(int yy, int xx) {
-		if(yy < 0 || yy >= map.length || xx < 0 || xx >= map[0].length || map[yy][xx].getSnakeNum() != 0)
-			return false;
-		return true;
 	}
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		for(int i = 0; i < map.length; i += 1) {
 			for(int j = 0; j < map[i].length; j += 1) {
-				Rect temp = map[i][j];
-				if(temp.getSnakeNum() == 0) g.setColor(new Color(254, 249, 239));					
-				else if(temp.getSnakeNum() == 1) g.setColor(new Color(254, 228, 64));
-				else if(temp.getSnakeNum() > 1) g.setColor(new Color(255, 134, 94));
-				else if(temp.getItem()) g.setColor(new Color(206, 229, 208));
-				g.drawRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
-				g.fillRect(temp.getX(), temp.getY(), temp.getWidth(), temp.getHeight());
+				System.out.print(map[i][j].getSnakeNum()+" ");
+				if(map[i][j].getSnakeNum() == 0) g.setColor(new Color(254, 249, 239));					
+				if(map[i][j].getSnakeNum() == 1) g.setColor(new Color(254, 228, 64));
+				if(map[i][j].getSnakeNum() >= 2) g.setColor(new Color(255, 134, 94));
+				if(map[i][j].getItem()) g.setColor(new Color(206, 229, 208));
+				g.drawRect(map[i][j].getX(), map[i][j].getY(), map[i][j].getWidth(), map[i][j].getHeight());
+				g.fillRect(map[i][j].getX(), map[i][j].getY(), map[i][j].getWidth(), map[i][j].getHeight());
 			}
 		}
 		repaint();
